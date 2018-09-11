2adc27804c433453eba5a171d9b4785602c502a6
侯光强 <houguangqiang@shsnc.com>
2018-09-11 19:18:58 +0800

    Nothing

--- src/main/java/bootwildfly/helper/MiMiApi.java
+++ src/main/java/bootwildfly/helper/MiMiApi.java
@@ -104,19 +104,4 @@ public class MiMiApi {
 
     }
 
-    public static void main(String[] args) {
-        String headers = ":authority: v3.mimi.ooo\n" +
-                ":method: GET\n" +
-                ":path: /user\n" +
-                ":scheme: https\n" +
-                "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n" +
-                "accept-encoding: gzip, deflate, br\n" +
-                "accept-language: zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,mg;q=0.6\n" +
-                "cookie: UM_distinctid=1656ab9ce0443-07d8731196259d-9393265-100200-1656ab9ce0525; _ga=GA1.2.2065632965.1535093887; __cfduid=d10a0541a82550431b3ee64854b5331d01536030510; PHPSESSID=8v545eq7ct9092s4egel6lkgnr; uid=137; email=1641438574%40qq.com; key=4c148456ff08fd00eb8610ed70dfff955844e3bcf2c9f; ip=207699b04f3b203ea26ffa531f0f5dc6; expire_in=1536739770\n" +
-                "referer: https://v3.mimi.ooo/auth/login\n" +
-                "upgrade-insecure-requests: 1\n" +
-                "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
-        String gacheck = checkin(parseHeaders(headers));
-        System.out.println(gacheck);
-    }
 }
